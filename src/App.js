1b8da2741dde36fdb0aa15627eca62bb5ccacd8d
Raimundas Radavicius <raimundas_radavicius@yahoo.com>
2023-05-18 10:37:27 +0100

    making noteslist and notes dynamic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is the note text 1",
+      date: "18/05/2023",
+    },
+    {
+      id: nanoid(),
+      text: "This is the note text 2",
+      date: "29/03/2023",
+    },
+    {
+      id: nanoid(),
+      text: "This is the note text 3",
+      date: "11/01/2023",
+    },
+  ]);
+
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   );
 };
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,11 +1,11 @@
 import { MdDeleteForever } from "react-icons/md";
 
-const Note = () => {
+const Note = ({ id, text, date }) => {
   return (
     <div className="note">
-      <span>This is my first note</span>
+      <span>{text}</span>
       <div className="note-footer">
-        <small>16/05/2023</small>
+        <small>{date}</small>
         <MdDeleteForever className="delete-icon" size="1.3em" />
       </div>
     </div>
